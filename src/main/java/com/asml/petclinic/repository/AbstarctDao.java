362662864a195954ccb16dc8a9001fde92e84e59
Mani Satpathy <53273021+Sunita238@users.noreply.github.com>
2021-12-16 21:13:37 +0100

    initial project commit
    initial project commit

---
+++ src/main/java/com/asml/petclinic/repository/AbstarctDao.java
@@ -0,0 +1,28 @@
+package com.asml.training.repository;
+
+import com.asml.training.entity.Employee;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+
+@Component
+public abstract class AbstarctDao<T> {
+
+    public final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("local");
+    public void add(T e){
+        EntityManager manager = entityManagerFactory.createEntityManager();
+        EntityTransaction tx = manager.getTransaction();
+        tx.begin();
+        manager.persist(e);
+        tx.commit();
+    }
+
+    public void update(T e){
+        EntityManager manager = entityManagerFactory.createEntityManager();
+		EntityTransaction tx = manager.getTransaction();
+        tx.begin();
+        manager.merge(e);
+		tx.commit();
+    }
+	
+}
