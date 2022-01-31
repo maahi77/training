c82b49170a01ea45f4cbe29c7603d1555d57f3af
maahi77 <86926070+maahi77@users.noreply.github.com>
2022-01-31 14:46:37 +0530

    Delete petclinic directory

--- petclinic/src/main/java/com/asml/petclinic/repository/PetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.asml.petclinic.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.asml.petclinic.model.Pet;
-
-public interface PetRepository extends JpaRepository<Pet, Integer>{
-
-}
